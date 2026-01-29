2328b014d2402157b513fb7cad0598be3ccd7fff
Mourão <p3dr0sk0sk7160@gmail.com>
2026-01-29 16:23:32 -0300

    movida função exibirOsLivrosNaTela para metodoForEach.js

--- app/main.js
+++ app/main.js
@@ -1,6 +1,5 @@
 let livros = [];
 const endpointDaApi = "https://guilhermeonrails.github.io/casadocodigo/livros.json";
-const livrosNaTela = document.getElementById("livros");
 
 getBuscarLivrosDaApi();
 
@@ -9,22 +8,4 @@ async function getBuscarLivrosDaApi() {
     livros = await resposta.json();
     console.table(livros);
     exibirOsLivrosNaTela(livros);
-};
-
-function exibirOsLivrosNaTela(listaDeLivros) {
-    listaDeLivros.forEach(livro => {
-        livrosNaTela.innerHTML += 
-        `<div class="livro">
-        <img class="livro__imagens" src="${livro.imagem}"
-            alt="${livro.alt}" />
-        <h2 class="livro__titulo">
-            ${livro.titulo}
-        </h2>
-        <p class="livro__descricao">${livro.autor}</p>
-        <p class="livro__preco" id="preco">R$${livro.preco}</p>
-        <div class="tags">
-            <span class="tag">${livro.categoria}</span>
-        </div>
-        </div>`
-    });
 };
