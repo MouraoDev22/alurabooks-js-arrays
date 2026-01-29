5ddba165f86fdfc5cf55f4588d171a68bd521ec4
Mourão <p3dr0sk0sk7160@gmail.com>
2026-01-29 01:30:42 -0300

    adicionado script para realizar requisição dos livros em json

---
+++ app/main.js
@@ -0,0 +1,10 @@
+let livros = [];
+const endpointDaApi = "https://guilhermeonrails.github.io/casadocodigo/livros.json";
+
+getBuscarLivrosDaApi();
+
+async function getBuscarLivrosDaApi() {
+    const resposta = await fetch(endpointDaApi);
+    livros = await resposta.json();
+    console.table(livros);
+}
